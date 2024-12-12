8d92840ded012540dea844a6519cae73527c8397
REXGAMING <122719724+attechlk@users.noreply.github.com>
2024-12-12 19:27:42 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94784604369"
+const SESSION_ID = "PRABATH-MD~onkE0LJb#IwOF5HMGqZAOHrXff3A_mXd_rioGVFKjz9Izj6r0-zg"
 
 
 module.exports = {
